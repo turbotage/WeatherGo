faa24f99df0e511c9659602cdf09ce9e55e7762c
TurboTage <viktor.vigren@gmail.com>
2020-11-20 00:37:23 +0100

    yas

--- weathergo.go
+++ weathergo.go
@@ -1,12 +1,12 @@
 package weather
 
 import (
-	"WeatherGo/fetcher"
+	"github.com/turbotage/WeatherGo/fetcher"
 )
 
 func main() {
 
-	go fetcher.BeginFetch("Weather!212", "/dev/ttyACM0", 9600)
+	go fetcher.BeginFetching("Weather!212", "/dev/ttyACM0", 9600)
 
 	//go server.BeginServer()
 }
