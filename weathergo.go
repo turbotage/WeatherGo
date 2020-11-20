149cbe1c14dca64775ee7f5dba49ad41381c723d
TurboTage <viktor.vigren@gmail.com>
2020-11-20 01:11:40 +0100

    4

--- weathergo.go
+++ weathergo.go
@@ -13,7 +13,7 @@ func main() {
 
 	fmt.Println("starting to fetch...")
 	wg.Add(1)
-	go fetcher.BeginFetching(&wg, "Weather!212", "/dev/ttyACM0/", 9600)
+	go fetcher.BeginFetching(&wg, "Weather!212", "/dev/ttyACM0", 9600)
 
 	fmt.Println("Main: Waiting for workers to finish")
 	wg.Wait()
